e5b62e3cd88768bde2698ddf6188a4febcf15b73
John Fouse <john.fouse@insight.com>
2021-04-05 11:31:27 -0400

    base component with child

--- src/main.js
+++ src/main.js
@@ -1,17 +1,47 @@
 import Vue from 'vue';
+import genres from './util/genres'
 import './style.scss';
 new Vue({
     el: '#app',
     components: {
         'movie-list': {
             template: `<div id="movie-list">
-                        Movie list            
-                        </div>`
+                            <div v-for="movie in movies" class="movie">
+                                {{movie.title}}
+                            </div>
+                        </div>`,
+            data: 
+                function() {
+                    return {
+                     movies: [
+                        { title: 'Pulp Fiction' },
+                        { title: 'Shawshank' },
+                        { title: 'Goodfellas' },
+                    ]
+                 }
+                    
+            }
+            
         },
         'movie-filter': {
+            data() {
+                return {
+                    //destructured assignment
+                    genres
+                }
+            },
             template: `<div id="movie-filter">
-                        Movie filter           
-                        </div>`
+                        <h2>Filter results</h2>
+                        <div class="filter-group">
+                            <check-filter v-for="genre in genres"></check-filter>
+                        </div>
+                        <check-filter></check-filter>        
+                        </div>`,
+            components: {
+                'check-filter': {
+                    template: `<div>Filter</div>`
+                }
+            }
         }
     },
 })

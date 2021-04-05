2b240a49d760b724e6a7e7ae04d9604c8ce8f95e
John Fouse <john.fouse@insight.com>
2021-04-05 16:29:12 -0400

    setup SFC, add API, output title of movies.

---
+++ src/components/MovieItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    {{ movie.movie.Title }}
+  </div>
+</template>
+<script>
+export default {
+  props: ['movie'],
+};
+</script>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="movie-list">
+    <movie-item
+      v-for="movie in filteredMovies"
+      v-bind:movie="movie"
+      class="movie"
+    ></movie-item>
+  </div>
+</template>
+<script>
+import MovieItem from './MovieItem.vue';
+export default {
+  components: { MovieItem },
+  props: ['genre', 'time', 'movies'],
+  methods: {
+    moviePassesGenreFilter(movie) {
+      if (!this.genre.length) {
+        return true;
+      } else {
+        return this.genre.find((genre) => movie.genre === genre);
+      }
+    },
+  },
+  computed: {
+    //https://vuejs.org/v2/guide/computed.html
+    filteredMovies() {
+      return this.movies.filter(this.moviePassesGenreFilter);
+    },
+  },
+};
+</script>

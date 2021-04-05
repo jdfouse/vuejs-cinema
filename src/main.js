57d8134176fcc21997d5ef06a46f96a02ce6c6a3
John Fouse <john.fouse@insight.com>
2021-04-05 11:02:55 -0400

    basic components

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue';
 import './style.scss';
 new Vue({
     el: '#app',
-    data: {
-        msg: 'Vue Project 2'
+    components: {
+        'movie-list': {
+            template: `<div id="movie-list">
+                        Movie list            
+                        </div>`
+        },
+        'movie-filter': {
+            template: `<div id="movie-filter">
+                        Movie filter           
+                        </div>`
+        }
     },
 })

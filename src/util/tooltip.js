08f64482b313ccec66387d82b3862ca3db479d7f
John Fouse <john.fouse@insight.com>
2021-04-06 17:45:12 -0400

    Add keep-alive

---
+++ src/util/tooltip.js
@@ -0,0 +1,38 @@
+//Vue.js custom plugin
+import { addClass, removeClass } from './helpers';
+
+let mouseOverHandler = function (event) {
+    let span = event.target.parentNode.getElementsByTagName('span')[0];
+    addClass(span, 'tooltip-show');
+}
+let mouseOutHandler = function (event) {
+    let span = event.target.parentNode.getElementsByTagName('span')[0];
+    removeClass(span, 'tooltip-show');
+}
+
+export default {
+    install(Vue) {
+        Vue.directive('tooltip', {
+            bind(el, bindings) {
+                console.log(bindings.value.seats);
+                let span = document.createElement('span');
+                let text = document.createTextNode(`Seats available: ${bindings.value.seats}`);
+                span.appendChild(text);
+                addClass(span, 'tooltip');
+                el.appendChild(span);
+                let div = el.getElementsByTagName('DIV')[0];
+                div.addEventListener('mouseover', mouseOverHandler);
+                div.addEventListener('mouseout', mouseOutHandler);
+                div.addEventListener('touchstart', mouseOverHandler);
+                div.addEventListener('touchend', mouseOutHandler);
+            },
+            unbind(el) {
+                let div = el.getElementsByTagName('DIV')[0];
+                div.removeEventListener('mouseover', mouseOverHandler);
+                div.removeEventListener('mouseout', mouseOutHandler);
+                div.removeEventListener('touchstart', mouseOverHandler);
+                div.removeEventListener('touchend', mouseOutHandler);
+            }
+        });
+    }
+}
